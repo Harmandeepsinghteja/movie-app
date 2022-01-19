3f6c7a0c1977d214b0db513f941bfafe7b33d656
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-01-19 21:53:03 +0530

    Actions creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,18 @@
-{
-    type: 'ADD_MOVIES',
-    movies: [m1,m2,m3]
+// {
+//     type: 'ADD_MOVIES',
+//     movies: [m1,m2,m3]
+// }
+
+
+
+// Action types
+export const ADD_MOVIES ='ADD_MOVIES';
+
+// Action creator
+export function addMovies (movies){
+    return {
+        type: ADD_MOVIES,
+        movies
+    }
 }
-{
-    type: 'DECREASE_COUNT'
-}
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
+import {ADD_MOVIES} from '../actions'
 export default function movie (state = [], action) {
     
-    if(action.type === 'ADD_MOVIES'){
+    if(action.type === ADD_MOVIES){
         return action.movies;
     }
     return state;
 }
+
