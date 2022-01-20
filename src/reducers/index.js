70cb6995e1dc2bafccbba6e407b7c4ddb88da94c
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-01-20 21:33:24 +0530

    Changing the State

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,18 @@
+import { bindActionCreators } from 'redux';
 import {ADD_MOVIES} from '../actions'
-export default function movie (state = [], action) {
+
+const initialMoviesState  = {
+    list: [],
+    favourites: []
+}
+
+export default function movie (state = initialMoviesState, action) {
     
     if(action.type === ADD_MOVIES){
-        return action.movies;
+        return {
+            ...state,
+            list: bindActionCreators.movies
+        }
     }
     return state;
 }
